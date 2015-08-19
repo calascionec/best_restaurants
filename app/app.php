e250385fb6eeb9b33ab9f3d0c584131554ba5280
Deron Johnson & Christopher Calascione <student@epicodus.com>
2015-08-19 14:33:19 -0700

    Add root route and index twig template

---
+++ app/app.php
@@ -0,0 +1,26 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Cuisine.php";
+    require_once __DIR__."/../src/Restaurant.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=best_restaurants';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    //Root route
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    return $app;
+?>
