329d38006deca67f1d383cdd0e388e672cef0ac7
Deron Johnson & Christopher Calascione <student@epicodus.com>
2015-08-19 10:03:15 -0700

    Add save and getAll methods to Cuisine class

--- src/Cuisine.php
+++ src/Cuisine.php
@@ -2,13 +2,13 @@
 
     class Cuisine
     {
-        private id;
         private name;
+        private id;
 
         function __contruct($name, $id = null)
         {
-            $this->id = $id;
             $this->name = $name;
+            $this->id = $id;
         }
 
         function getName()
@@ -16,17 +16,40 @@
             return $this->name;
         }
 
-        function getId()
-        {
-            return $this->id;
-        }
-
         function setName($new_name)
         {
             $this->name = $new_name;
         }
 
-        
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = $new_id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO cuisines (name) VALUES ('{$this->getName()}');");
+            $result_id = $GLOBALS['DB']->lastInsertId();
+            $this->setId($result_id);
+        }
+
+        static function getAll()
+        {
+            $returned_cuisines = $GLOBALS['DB']->query("SELECT * FROM cuisines;");
+            $cuisines = array();
+            foreach ($returned_cuisines as $cuisine) {
+                $name = $cuisine['name'];
+                $id = $cuisine['id'];
+                $new_cuisine = new Cuisine($name, $id);
+                array_push($cuisines, $new_cuisine);
+            }
+            return $cuisines;
+        }
     }
 
  ?>
